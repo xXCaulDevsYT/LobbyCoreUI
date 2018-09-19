85a9aaf4886e7251622ad97a2fdba0141c459fd6
manyDARK <31310430+manyDARK@users.noreply.github.com>
2018-09-19 06:45:38 +0200

    NavigatorUI finish!

--- src/many1337/Main.php
+++ src/many1337/Main.php
@@ -16,61 +16,96 @@ use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\Listeners;
 use pocketmine\utils\TextFormat;
 
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase implements Listener
+{
 
-	public function onEnable() {
-    		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-    	}
-    
-    	public function onDisable() {
-    		foreach($this->getServer()->getOnlinePlayers() as $p) {
-			$p->transfer("80.99.208.62", "19150");
-		}
-   	}
-    
-    	public function onJoin(PlayerJoinEvent $event) {
-    	
-        	$player = $event->getPlayer();
-        	$name = $player->getName();
-        	$this->Main($player);
-        	$event->setJoinMessage("§7[§9+§7] §9" . $name);
-        	
-    	}
-    	
-    	public function onQuit(PlayerQuitEvent $event) {
-    	
-        	$player = $event->getPlayer();
-        	$name = $player->getName();
-        	$event->setQuitMessage("§7[§c-§7] §c" . $name);
-        	
-    	}
-    	
-    	public function Main(Player $player) {
-        	$player->getInventory()->clearAll();
-        	$player->getInventory()->setItem(4, Item::get(345)->setCustomName(TextFormat::YELLOW."Navigator"));
-        	$player->getInventory()->setItem(8, Item::get(399)->setCustomName(TextFormat::GREEN."Infos"));
-            $player->getInventory()->setItem(0, Item::get(397 , 3)->setCustomName(TextFormat::AQUA."Profile"));
+    public function onEnable()
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
 
-       }
-    	
-    	public function onInteract(PlayerInteractEvent $event) {
-       		$player = $event->getPlayer();
-        	$item = $player->getInventory()->getItemInHand();
-        
-        	if($item->getCustomName() == TextFormat::YELLOW . "Navigator") {
-            		$menu = new Navigator($this);
-             		$menu->Navigator($player);  
-   
-             	}
+    public function onDisable()
+    {
+        foreach ($this->getServer()->getOnlinePlayers() as $p) {
+            $p->transfer("80.99.208.62", "19150");
+        }
+    }
 
-            if($item->getCustomName() == TextFormat::GREEN . "Lobbys") {
-             
-                 }
-                 
-           if($item->getCustomName() == TextFormat::AQUA . "Profil") {
-                  $menu = new Profil($this);
-             	    $menu->Profil($player);
-             	    
-	 }
-  }
+    public function onJoin(PlayerJoinEvent $event)
+    {
+
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        $this->Main($player);
+        $event->setJoinMessage("§7[§9+§7] §9" . $name);
+
+    }
+
+    public function onQuit(PlayerQuitEvent $event)
+    {
+
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        $event->setQuitMessage("§7[§c-§7] §c" . $name);
+
+    }
+
+    public function Main(Player $player)
+    {
+        $player->getInventory()->clearAll();
+        $player->getInventory()->setItem(4, Item::get(345)->setCustomName(TextFormat::YELLOW . "Navigator"));
+        $player->getInventory()->setItem(8, Item::get(399)->setCustomName(TextFormat::GREEN . "Infos"));
+        $player->getInventory()->setItem(0, Item::get(397, 3)->setCustomName(TextFormat::AQUA . "Profile"));
+
+    }
+
+    public function onInteract(PlayerInteractEvent $event)
+    {
+        $player = $event->getPlayer();
+        $item = $player->getInventory()->getItemInHand();
+
+        if ($item->getCustomName() == TextFormat::YELLOW . "Navigator") {
+            $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+            $form = $api->createSimpleForm(function (Player $sender, array $data) {
+                $result = $data[0];
+
+                if ($result === null) {
+                    return true;
+                }
+                switch ($result) {
+                    case 0:
+                        $command = "transferserver 127.0.0.1 1";
+                        $this->getServer()->getCommandMap()->dispatch($sender, $command);
+                        break;
+                    case 1:
+                        $command = "transferserver 127.0.0.1 2";
+                        $this->getServer()->getCommandMap()->dispatch($sender, $command);
+                        break;
+                    case 2:
+                        $command = "transferserver 127.0.0.1 3";
+                        $this->getServer()->getCommandMap()->dispatch($sender, $command);
+                        break;
+                    case 3:
+                        $command = "transferserver 127.0.0.1 4";
+                        $this->getServer()->getCommandMap()->dispatch($sender, $command);
+                        break;
+                    case 4:
+                        $command = "transferserver 127.0.0.1 5";
+                        $this->getServer()->getCommandMap()->dispatch($sender, $command);
+                        break;
+
+
+                }
+            });
+            $form->setTitle("§l§aServer Selector");
+            $form->setContent("Answer a server for teleporting");
+            $form->addButton(TextFormat::BOLD . "§aSkyBlock §7- §7[§cCLOSED§7]");
+            $form->addButton(TextFormat::BOLD . "§aFast§fBridge §7- §7[§cCLOSED§7]");
+            $form->addButton(TextFormat::BOLD . "§aSkyWars §7- §7[§cCLOSED§7]");
+            $form->addButton(TextFormat::BOLD . "§aBedWars §7- §7[§cCLOSED§7]");
+            $form->addButton(TextFormat::BOLD . "§aHungerGames §7- §7[§cCLOSED§7]");
+            $form->sendToPlayer($player);
+
+        }
+    }
 }
